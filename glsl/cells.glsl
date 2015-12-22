9d8b547790f96c44be4c66906b787669212305f4
Joseph Wilk <joe@josephwilk.net>
2015-12-22 16:22:19 +0100

    lines

--- glsl/cells.glsl
+++ glsl/cells.glsl
@@ -7,6 +7,30 @@ float hash(in float n)
     return fract(sin(n)*43758.5453123);
 }
 
+
+vec4 lineDistort(vec4 cTextureScreen, vec2 uv1){
+  float sCount = 900.;
+  float nIntensity=0.1;
+  float sIntensity=0.2;
+  float noiseEntry = 0.0;
+  float accelerator= 1000.0;
+
+  // sample the source
+  float x = uv1.x * uv1.y * iGlobalTime * accelerator;
+  x = mod( x, 13.0 ) * mod( x, 123.0 );
+  float dx = mod( x, 0.05 );
+  vec3 cResult = cTextureScreen.rgb + cTextureScreen.rgb * clamp( 0.1 + dx * 100.0, 0.0, 1.0 );
+  // get us a sine and cosine
+  vec2 sc = vec2( sin( uv1.y * sCount ), cos( uv1.y * sCount ) );
+  // add scanlines
+  cResult += cTextureScreen.rgb * vec3( sc.x, sc.y, sc.x ) * sIntensity;
+
+  // interpolate between source and result by intensity
+  cResult = cTextureScreen.rgb + clamp(nIntensity, noiseEntry,1.0 ) * (cResult - cTextureScreen.rgb);
+
+  return vec4(cResult, cTextureScreen.a);
+}
+
 void main() {
 
     vec2 xy = ( ( gl_FragCoord.xy / resolution.xy  )- .5) * 2.;
@@ -38,6 +62,7 @@ void main() {
         }
     }
 
-    gl_FragColor = vec4( pp + vec3( 1.) * ( 1. - max( 0.0, dot( pp, lightDir)) ), 1. );
+    vec4 o = vec4( pp + vec3( 1.) * ( 1. - max( 0.0, dot( pp, lightDir)) ), 1. );
+    gl_FragColor = lineDistort(o, xy);
 
 }
